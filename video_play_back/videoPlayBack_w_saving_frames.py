a5fd5ab4d9e6ec9f9e9254111f193fa4627dca10
Walid Ahmed <walidahmed@Walids-MacBook-Air.local>
2019-11-01 16:35:00 -0400

    initial commit

--- video_play_back/videoPlayBack_w_saving_frames.py
+++ video_play_back/videoPlayBack_w_saving_frames.py
@@ -1,59 +1,57 @@
+
+#cd video_play_back/
+#python  video_play_back/videoPlayBack_w_saving_frames.py   --fileName videos/CarsDrivingUnderBridge.mp4 --everyNFrame 1
+
+#Frames will be samples everyNFrame and saved to a folder with the same name as the video file name
+
+
 import numpy as np
 import cv2
 import os
 import shutil
+import argparse
 
 
 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("--fileName", required=True,help="path to video file")
+ap.add_argument("--everyNFrame", default=1,help="path to input image")
 
-fileName="videos/lorex-20181217-143851-144310.mp4"
+
+
+args = vars(ap.parse_args())
+
+fileName=args["fileName"]
+everyNFrame=int(args["everyNFrame"])
+
 folderNameToSaveFrames=(os.path.basename(fileName))[:-4]
-
-
 
 
 cap = cv2.VideoCapture(fileName)
 shutil.rmtree(folderNameToSaveFrames, ignore_errors=True, onerror=None)
-if not os.path.exists(folderNameToSaveFrames):
-    			os.makedirs(folderNameToSaveFrames)
+os.makedirs(folderNameToSaveFrames)
 
-width=cap.get(3) #width
-height=cap.get(4) #height
-#ret=cap.set(3,300) 
-#ret=cap.set(4,300)
-#fps    = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-#length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-#width  = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
-#height = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
-#print("FRAME_Per_Second",fps)
 
-print (width,height)
-fps = cap.get(cv2.CAP_PROP_FPS)
-print ("Frames per second  : {0}".format(fps))
 frameNum=0
-numberOfFramesPerSecondtoSave=6
-everyNFrame=int(fps)*2
-everyNFrame=int(fps/numberOfFramesPerSecondtoSave)
 
-everyNFrame=5
-print("Saving  a frame from every {0} frames ".format(everyNFrame))
-raw_input("press any key to continue")
+
+print("[INFO] Saving  a frame from every {} frames from video file {} ".format(everyNFrame,fileName))
+input("press any key to continue")
 while(cap.isOpened()):
     ret, frame = cap.read()
     if (frame is None):
-	break
+        break
     if(frameNum%everyNFrame==0):
-    	cv2.imwrite(os.path.join(folderNameToSaveFrames, folderNameToSaveFrames+"_"+str(frameNum)+".png"), frame)
-    	print("framed saved to  "+ "frame"+"_"+str(frameNum)+".png")
+        fileNameToSaveFrame=os.path.join(folderNameToSaveFrames, folderNameToSaveFrames+"_"+str(frameNum)+".png")
+        cv2.imwrite(fileNameToSaveFrame, frame)
+        print("[INFO] frame#  {}  saved to  {}".format(frameNum,fileNameToSaveFrame))
+    else:  
+        print("[INFO]  frame# {}  skipped and not saved".format(frameNum))
+  
     
-    
-    
-    
-    
-    #cv2.imwrite("frame"+"_"+str(i)+".png", frame)
     
     frameNum=frameNum+1
-    print(frameNum)
 
 
     cv2.imshow('frame',frame)
@@ -62,3 +60,5 @@ while(cap.isOpened()):
         break
 cap.release()
 cv2.destroyAllWindows()
+print("[INFO] Extracted frames saved to folder {}".format(folderNameToSaveFrames))
+
